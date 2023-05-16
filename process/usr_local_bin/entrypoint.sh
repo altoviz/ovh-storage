7057c3a26af3206e955644b894960c3800875cbc
Alex Danvy <alex@danvy.tv>
2023-05-16 10:35:25 +0200

    Hide rclone config output

--- process/usr_local_bin/entrypoint.sh
+++ process/usr_local_bin/entrypoint.sh
@@ -5,7 +5,8 @@ rclone config update ovh \
   access_key_id=$OVH_S3_ACCESS_KEY_ID \
   secret_access_key=$OVH_S3_SECRET_ACCESS_KEY \
   endpoint=https://s3.$OVH_S3_REGION.io.cloud.ovh.net \
-  region=$OVH_S3_REGION location_constraint=$OVH_S3_REGION
+  region=$OVH_S3_REGION location_constraint=$OVH_S3_REGION \
+  > /dev/nul 2>&1
 echo "Starting Cron"
 cat /tmp/crontab > /etc/crontabs/root
 # Foreground LogLevel 8
